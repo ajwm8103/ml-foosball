8e4f81704b9b3105eabe48bd3fa6b992826765f9
Andrew Magnuson <ajwm8103@gmail.com>
2022-10-18 12:01:18 -0400

    Porting more code

--- Assets/FoosballEnvController.cs
+++ Assets/FoosballEnvController.cs
@@ -2,6 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum BoardType { ENGSCI };
+public enum RulesType { ENGSCI, GLOBAL };
+public enum PlayerType { PLAYER1, PLAYER2, STAND, BOT};
+public enum DiscreteActionKey { GOALIE_HOLD, DEFENDERS_HOLD, MIDFIELDERS_HOLD, OFFENSIVE_HOLD };
+public enum ContinuousActionKey { GOALIE_TORQUE, GOALIE_FORCE, DEFENDERS_TORQUE, DEFENDERS_FORCE, MIDFIELDERS_TORQUE, MIDFIELDERS_FORCE, OFFENSIVE_TORQUE, OFFENSIVE_FORCE };
 public class FoosballEnvController : MonoBehaviour
 {
     // Match Params
@@ -10,7 +15,28 @@ public class FoosballEnvController : MonoBehaviour
 
     // Match details
     [Header("Match Stats")]
+    public List<AgentInfo> agents;
     public int totalSteps;
+
+    [System.Serializable]
+    public class AgentInfo
+    {
+        public int points = 0; // until ma
+        public FoosballAgent agent;
+        [HideInInspector]
+        public Rigidbody2D rigidbody;
+        [HideInInspector]
+        public float totalReward;
+        public int totalWins = 0;
+
+        public void AddReward(float x)
+        {
+            agent.AddReward(x);
+            totalReward += x;
+        }
+    }
+    private AgentInfo m_blueAgent;
+    private AgentInfo m_redAgent;
 
 
     // Start is called before the first frame update
@@ -23,5 +49,27 @@ public class FoosballEnvController : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void ResetEnv()
+    {
+        totalSteps = 0;
+        firstTeam = Random.Range(0, 2) == 1 ? Team.RED : Team.BLUE;
+
+        // Reset Agents
+        foreach (AgentInfo agentInfo in agents)
+        {
+            //Debug.Log(string.Format("{0} {1}", agentInfo.agent.name, agentInfo.totalReward));
+            //var randomPosX = Random.Range(-5f, 5f);
+            agentInfo.points = 0;
+            agentInfo.totalReward = 0f;
+            agentInfo.agent.SetAgentInfo(agentInfo);
+            agentInfo.agent.ResetLegend();
+            agentInfo.agent.isAlive = true;
+            // Do something for respawning, randomize spawn points + instant spawn at start
+
+            agentInfo.rigidbody.velocity = Vector3.zero;
+            agentInfo.rigidbody.angularVelocity = 0f;
+        }
     }
 }
--- Assets/Scripts/FoosballAgent.cs
+++ Assets/Scripts/FoosballAgent.cs
@@ -10,10 +10,16 @@ public class FoosballAgent : Agent
 
     private float m_Existential;
 
+    [Header("Game Stats")]
+    [SerializeField]
+    public PlayerType playerType = PlayerType.PLAYER1;
+
     // Private vars
 
     // References to components
     FoosballEnvController envController;
+    FoosballSettings m_foosballSettings;
+    FoosballEnvController.AgentInfo m_agentInfo;
 
     EnvironmentParameters m_ResetParams;
 
@@ -35,6 +41,29 @@ public class FoosballAgent : Agent
         }
     }
 
+    // cause i basically need an input for each rod to control the spin. so i'm thinking grip + torque, both with constraints to human levels
+    // but...how does grip work..???? high grip + high torque means?
+
+    // grip is how tightly you're holding the rod. cause if i'm not holding it, then it'll just go weeeee
+    // but if i am holding it, it'll stay where it is..???? not sure honestly.
+    // cause like, if i want it to stay in a spot i'm gripping, when it gets hit i automatically apply a torque..? or like a spring constant i guess
+    // but when i'm moving it i'm applying a more direct torque, but also have a high spring constant in case i get hit.?
+    // so perhaps torque is torque and grip is spring constant on my grip
+    // so high torque necessitates high grip, but high grip doesn't necessitate high torque...
+    // idk. just trying to get all the simulation dynamics of real play.
+
+    // what if i increase the torque a ton so the angular velocity is high. then let go? and want it to keep spinning?
+    // that would mean i have zero applied torque, sure. but my grip would instantly stop me.
+    // i like that actually. ok ok.
+    // so
+    //        high torque                  no torque
+    // grip   does the torque with high k  stays still with high k
+
+    // no grip floppy mode                 "        "
+
+    // i'll try grip as a toggle then. and torque as a value w/ a clamp.
+    // also grip will have a min time. so it takes time to change grip and we can't just spam it per frame.
+
     public override void Initialize()
     {
         base.Initialize();
@@ -48,6 +77,11 @@ public class FoosballAgent : Agent
         m_ResetParams = Academy.Instance.EnvironmentParameters;
 
         ResetAgent();
+    }
+
+    public void SetAgentInfo(BrawlEnvController.AgentInfo x)
+    {
+        m_agentInfo = x;
     }
 
     public void ResetAgent()
@@ -81,15 +115,113 @@ public class FoosballAgent : Agent
         return vs;
     }
 
-    // Start is called before the first frame update
-    void Start()
+    public override void OnActionReceived(ActionBuffers actionBuffers)
     {
-        
+        // Actions, size = 9
+
+        //m_agentInfo.AddReward(-m_Existential);
+        //AddReward(-m_Existential);
+        if (Mathf.Abs(transform.position.x) > 5f)
+        {
+            m_agentInfo.AddReward(-m_Existential);
+        }
+
+        //MoveAgent(actionBuffers.DiscreteActions, actionBuffers.ContinuousActions);
+
+        // Graphics
+        if (m_foosballSettings.effectsAmount != 0)
+        {
+            m_damageText.text = damage.ToString();
+            if (stunFrames > 0)
+            {
+                m_legendSprite.color = stunColor;
+                //m_capsuleSprite.color = stunColor;
+            }
+            else if (m_sprinting)
+            {
+                m_legendSprite.color = sprintingColor;
+                //m_capsuleSprite.color = sprintingColor;
+            }
+            else
+            {
+                m_legendSprite.color = defaultColor;
+                //m_capsuleSprite.color = defaultColor;
+            }
+
+            // Facing
+            m_legendSprite.flipX = facingDirection == -1;
+
+            envController.DisplayAction(team, teamPosition, actionBuffers.DiscreteActions);
+        }
+
+        // Rewards
+
+        if (isAlive)
+        {
+            if (bottomKO || leftKO || rightKO)
+            {
+                // Fell off of bottom
+                KO();
+            }
+            else if (topKO && stunFrames != 0)
+            {
+                KO();
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void Heuristic(in ActionBuffers actionsOut)
     {
-        
+        var discreteActionsOut = actionsOut.DiscreteActions;
+        var continuousActionsOut = actionsOut.ContinuousActions;
+        discreteActionsOut[(int)DiscreteActionKey.GOALIE_HOLD] = 1;
+        discreteActionsOut[(int)DiscreteActionKey.DEFENDERS_HOLD] = 1;
+        discreteActionsOut[(int)DiscreteActionKey.MIDFIELDERS_HOLD] = 1;
+        discreteActionsOut[(int)DiscreteActionKey.OFFENSIVE_HOLD] = 1;
+
+        if (playerType == PlayerType.PLAYER1)
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                continuousActionsOut[(int)ContinuousActionKey.GOALIE_FORCE] = 1;
+                continuousActionsOut[(int)ContinuousActionKey.DEFENDERS_FORCE] = 1;
+                continuousActionsOut[(int)ContinuousActionKey.MIDFIELDERS_FORCE] = 1;
+                continuousActionsOut[(int)ContinuousActionKey.OFFENSIVE_FORCE] = 1;
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                continuousActionsOut[(int)ContinuousActionKey.GOALIE_FORCE] = -1;
+                continuousActionsOut[(int)ContinuousActionKey.DEFENDERS_FORCE] = -1;
+                continuousActionsOut[(int)ContinuousActionKey.MIDFIELDERS_FORCE] = -1;
+                continuousActionsOut[(int)ContinuousActionKey.OFFENSIVE_FORCE] = -1;
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                continuousActionsOut[(int)ContinuousActionKey.GOALIE_TORQUE] = 1;
+                continuousActionsOut[(int)ContinuousActionKey.DEFENDERS_TORQUE] = 1;
+                continuousActionsOut[(int)ContinuousActionKey.MIDFIELDERS_TORQUE] = 1;
+                continuousActionsOut[(int)ContinuousActionKey.OFFENSIVE_TORQUE] = 1;
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                continuousActionsOut[(int)ContinuousActionKey.GOALIE_TORQUE] = -1;
+                continuousActionsOut[(int)ContinuousActionKey.DEFENDERS_TORQUE] = -1;
+                continuousActionsOut[(int)ContinuousActionKey.MIDFIELDERS_TORQUE] = -1;
+                continuousActionsOut[(int)ContinuousActionKey.OFFENSIVE_TORQUE] = -1;
+            }
+        }
+        else if (playerType == PlayerType.PLAYER2)
+        {
+            // idk controller support?
+        }
+        else if (playerType == PlayerType.STAND)
+        {
+            // do nothing hurrah
+        }
+        else if (playerType == PlayerType.BOT)
+        {
+
+        }
     }
+
 }
