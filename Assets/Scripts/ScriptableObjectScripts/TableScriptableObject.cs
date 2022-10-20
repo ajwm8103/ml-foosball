bf826a1fc662b5d64854bdacf889879599adab82
Andrew Magnuson <ajwm8103@gmail.com>
2022-10-20 14:14:05 -0400

    Table scriptableobject made

--- Assets/Scripts/ScriptableObjectScripts/TableScriptableObject.cs
+++ Assets/Scripts/ScriptableObjectScripts/TableScriptableObject.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public struct ErrorFloat
 {
     public float average;
@@ -13,14 +14,30 @@ public struct ErrorFloat
     }
 }
 
-[CreateAssetMenu(fileName = "Legend Configuration", menuName = "ScriptableObject/Legend Configuration", order = 1)]
+[CreateAssetMenu(fileName = "Table Configuration", menuName = "ScriptableObject/Table Configuration", order = 1)]
 public class TableScriptableObject : ScriptableObject
 {
+    // Lengths in metres
+    [Header("Table Properties")]
     public ErrorFloat tableLength = new ErrorFloat(1.42f, 0.01f);
     public ErrorFloat tableWidth = new ErrorFloat(0.77f, 0.005f);
     public ErrorFloat tableDepth = new ErrorFloat(0.10f, 0.005f);
+    public bool slopes = false;
+    public ErrorFloat kBumper = new ErrorFloat(0.10f, 0.005f); //
+
+    [Header("Rod and Position Properties")]
     public ErrorFloat rodDiameter = new ErrorFloat(0.022f, 0.0005f);
+
+    [Header("Foosman Properties")]
     public ErrorFloat foosmanDepth = new ErrorFloat(0.044f, 0.0002f);
     public ErrorFloat foosmanWidth = new ErrorFloat(0.038f, 0.0002f);
-    public ErrorFloat foosmanBodyHeight = new ErrorFloat(0.038f, 0.0002f);
+    public ErrorFloat foosmanBodyHeight = new ErrorFloat(0.044f, 0.0002f);
+    public ErrorFloat foosmanFootHeight = new ErrorFloat(0.01f, 0.0001f);
+    public ErrorFloat foosmanFootWidth = new ErrorFloat(0.023f, 0.0001f);
+    public ErrorFloat foosmanShoulderHeight = new ErrorFloat(0.014f, 0.0001f);
+    public ErrorFloat foosmanHeadHeight = new ErrorFloat(0.021f, 0.0001f);
+    public ErrorFloat foosmanHeadWidth = new ErrorFloat(0.015f, 0.0001f);
+    public ErrorFloat foosmanHeadDepth = new ErrorFloat(0.015f, 0.0001f);
+
+
 }
--- Assets/Scripts/Table.cs
+++ Assets/Scripts/Table.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Table : MonoBehaviour
 {
     [Header("Table Parameters")]
+    public TableScriptableObject tso;
 
     // Start is called before the first frame update
     void Start()
